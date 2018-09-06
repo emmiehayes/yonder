28b6eaaa23d248c6e91a7e09fc6042b099675874
Emmie Hayes <emmiehayes2@gmail.com>
2018-09-06 15:31:46 -0600

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'sessions#new'
+  
+  root                             to: 'home#index'
+  get '/auth/:provider/callback',  to: 'sessions#create'
+  get '/auth/failure',             to: redirect('/')
+  get 'logout',                    to: 'sessions#destroy',  as: 'logout'
+
+   get '/dashboard', to: 'dashboard#show'
 end
