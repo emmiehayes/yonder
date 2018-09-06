76e687528a545d514a9655f93119004e8f53762c
Emmie Hayes <emmiehayes2@gmail.com>
2018-09-06 15:59:34 -0600

    retro

--- app/controllers/dashboard_controller.rb
+++
@@ -1,7 +0,0 @@
-class DashboardController < ApplicationController
-
-  def show 
-    @google_user = Google::UserSearch.new(current_user.oauth_token, current_user.username).find_user
-  end
-  
-end
