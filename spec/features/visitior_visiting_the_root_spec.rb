abd74e8e721a54d47cd8232acdfe7c9d313f8cdd
Emmie Hayes <emmiehayes2@gmail.com>
2018-09-05 18:15:29 -0600

    test landing page only, add route, session controller and session new view page

--- spec/features/visitior_visiting_the_root_spec.rb
+++ spec/features/visitior_visiting_the_root_spec.rb
@@ -1,23 +1,11 @@
 require 'rails_helper'
 
-describe 'registration workflow' do
-  it 'submits registration succesfully' do
+describe 'visitor visiting "/"' do
+  it "will see a landing page" do
 
-    name = 'Emmie'
-    email_address = 'Emmie@google.com'
-    password = 'passwordEmmie'
+    visit "/"
 
-    visit '/'
-    click_on 'Register'
-
-    expect(current_path).to eq(new_user_path)
-
-    fill_in 'user[name]', with: name
-    fill_in 'user[email]', with: email_address
-    fill_in 'user[password]', with: password
-    click_on 'Submit'
-
-    expect(current_path).to eq(user_path(User.last))
-    expect(page).to have_content("Logged in as: #{User.last.name}")
+    expect(page).to have_content("Yonder")
+    expect(page).to have_content("Go where the weather takes you")
   end
 end
