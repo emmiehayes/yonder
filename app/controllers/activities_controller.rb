3a4eea51d61ac4b6e95e886ab761c0f25711ba73
Emmie Hayes <emmiehayes2@gmail.com>
2018-09-08 20:29:15 -0600

    all test pass, refactor activiies controller and update views to call helper in view, push work down the stack

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,9 @@
 class ActivitiesController < ApplicationController
+helper_method :activity
 
-  def new
-    @activity = Activity.new
+  def activity
+    #not to be called in view, used because the new, show, edit methods all kick off this helper method
+    @memoized_activity ||= Activity.find_or_initialize_by(id: params[:id])
   end
 
   def create
@@ -13,6 +15,24 @@ class ActivitiesController < ApplicationController
       flash[:alert] = "Missing required fields, activity was not created."
       render :new
     end
+  end
+
+  def update 
+    @activity = Activity.find(params[:id])
+    @activity.update(activity_params)
+    if @activity.save
+      flash[:successs] = "Activity successfully updated."
+      redirect_to user_activity_path(current_user, @activity)
+    else
+      flash[:alert] = "Missing required fields, activity was not updated."
+      render :edit
+    end
+  end
+
+  def destroy 
+    activity = Activity.destroy(params[:id])
+    flash[:success] = "Successfully deleted #{activity.title}"
+    redirect_to dashboard_path
   end
 
   private
