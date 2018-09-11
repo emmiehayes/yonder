d2ba6251e84c57b9f5eda5fab8c2e98594924a6c
Emmie Hayes <emmiehayes2@gmail.com>
2018-09-11 17:40:45 -0600

    seed small data set for dev

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+activity_1 = Activity.create(title: "Mountain Biking")
+pin_1 = Pin.create(city: "Fruita", state: "Colorado", latitude: 39.1589, longitude: 108.7290)
+activity_pin_1 = ActivityPin.create(activity_id: 1, pin_id: 1)
+
+activity_2 = Activity.create(title: "Rock Climbing")
+pin_2 = Pin.new(city: "Golden", state: "Colorado", latitude: 39.7555, longitude: 105.2211)
+activity_pin_2 = ActivityPin.create(activity_id: 2, pin_id: 2)
+
+activity_3 = Activity.create(title: "Hiking")
+pin_3 = Pin.new(city: "Boulder", state: "Colorado", latitude: 40.0150, longitude: 105.2705)
+activity_pin_3 = ActivityPin.create(activity_id: 3, pin_id: 3)
+
+
+activity_4 = Activity.create(title: "Skiing")
+pin_4 = Pin.new(city: "Breckenridge", state: "Colorado", latitude: 39.4817, longitude: 106.0384)
+activity_pin_4 = ActivityPin.create(activity_id: 4, pin_id: 4)
+
