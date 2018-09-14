112ed6484ae57609c5c2ae6bd1598cef9d008e3c
Emmie Hayes <emmiehayes2@gmail.com>
2018-09-14 10:29:25 -0600

    update controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,7 @@
 class DashboardController < ApplicationController
 
   def index
-    if current_user
-      @user = User.find(current_user[:id])
-    end
+    @user = User.find(current_user[:id])
   end
 
   def show 
